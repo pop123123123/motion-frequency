b6a1a5374d059198b03f44b1f465ee858f40f323
Louis MOUHAT <Louis.Mouhat-Courvoisier@grenoble-inp.org>
2019-05-10 14:42:16 +0200

    reformat, add gitignore

--- cv_samples/opt_flow.py
+++ cv_samples/opt_flow.py
@@ -25,8 +25,8 @@ import video
 
 def draw_flow(img, flow, step=16):
     h, w = img.shape[:2]
-    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
-    fx, fy = flow[y,x].T
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1).astype(int)
+    fx, fy = flow[y, x].T
     lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines + 0.5)
     vis = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
@@ -38,13 +38,13 @@ def draw_flow(img, flow, step=16):
 
 def draw_hsv(flow):
     h, w = flow.shape[:2]
-    fx, fy = flow[:,:,0], flow[:,:,1]
+    fx, fy = flow[:, :, 0], flow[:, :, 1]
     ang = np.arctan2(fy, fx) + np.pi
     v = np.sqrt(fx*fx+fy*fy)
     hsv = np.zeros((h, w, 3), np.uint8)
-    hsv[...,0] = ang*(180/np.pi/2)
-    hsv[...,1] = 255
-    hsv[...,2] = np.minimum(v*4, 255)
+    hsv[..., 0] = ang*(180/np.pi/2)
+    hsv[..., 1] = 255
+    hsv[..., 2] = np.minimum(v*4, 255)
     bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
     return bgr
 
@@ -52,16 +52,18 @@ def draw_hsv(flow):
 def warp_flow(img, flow):
     h, w = flow.shape[:2]
     flow = -flow
-    flow[:,:,0] += np.arange(w)
-    flow[:,:,1] += np.arange(h)[:,np.newaxis]
+    flow[:, :, 0] += np.arange(w)
+    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
     res = cv.remap(img, flow, None, cv.INTER_LINEAR)
     return res
 
+
 def running_mean(x, N):
-    #cumsum = np.cumsum(np.insert(x, 0, 0)) 
-    #return (cumsum[N:] - cumsum[:-N]) / float(N)
+    #cumsum = np.cumsum(np.insert(x, 0, 0))
+    # return (cumsum[N:] - cumsum[:-N]) / float(N)
     conv = np.convolve(x, np.ones((N,))/N, mode='valid')
     return np.concatenate((conv, x[conv.shape[0] - x.shape[0]:]))
+
 
 def main():
     import sys
@@ -92,7 +94,8 @@ def main():
         #gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)[w:2*w,h:2*h]
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-        flow = cv.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        flow = cv.calcOpticalFlowFarneback(
+            prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         prevgray = gray
 
         data.append(np.absolute(flow).mean(axis=(0,1)))
